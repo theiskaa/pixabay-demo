a01c31ca27c9c5ead1c990755aaf8e2ce88871eb
Ismael Sh <me@theiskaa.com>
2024-09-13 12:52:12 +0400

    feat(app): add sample application class

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// TODO(1): 1. add router
+// TODO(2): 2. add localization
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pixaby Demo',
+    );
+  }
+}
